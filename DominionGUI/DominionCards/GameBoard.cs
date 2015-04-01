b23c0cb989475d4c3868e1a7335abf43eed28caf
bonattt <bonattt@rose-hulman.edu>
2015-04-01 00:04:32 -0400

    Added more classes

---
+++ DominionGUI/DominionCards/GameBoard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominionCards
+{
+    class GameBoard
+    {
+        Queue<Player> turnOrder;
+        Dictionary<Card, int> cards;
+        public GameBoard(Dictionary<Card, int> cards, Queue<Player> turnOrder)
+        {
+            // TODO implement
+        }
+    }
+}
