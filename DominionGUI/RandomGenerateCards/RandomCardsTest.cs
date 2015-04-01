310fff846a984ab1a51c6d641ebe29367c31780c
Luke Zhang <zhangl@rose-hulman.edu>
2015-04-01 17:01:50 -0400

    Create RandomCards Tests Class
    Delete RandomCards Tests Project

---
+++ DominionGUI/RandomGenerateCards/RandomCardsTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace RandomGenerateCards
+{
+    [TestFixture()]
+    class RandomCardsTest
+    {
+        [Test()]
+        public void testRandom1()
+        {
+            Boolean israndom = false;
+            Assert.AreEqual(true,israndom);
+        }
+
+    }
+}
+
