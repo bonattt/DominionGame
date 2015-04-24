b1c09e0f00deed1a96334ba662af01a7f66b3f15
bonattt <bonattt@rose-hulman.edu>
2015-04-24 11:40:16 -0400

    converted some tests

---
+++ DominionGUI/UnitTestProject2/AddCardsToGameBoardUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using DominionCards;
+using DominionCards.KingdomCards;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class AddCardsToGameBoardUnitTest
+    {
+        [TestMethod]
+        public void testActionCardAddedToBoard()
+        {
+            Dictionary<Card, int> cards = new Dictionary<Card, int>();
+            Queue<Player> turns = new Queue<Player>();
+            GameBoard board = new GameBoard(cards, turns);
+            Card newCard = new Village();
+            board.addCard(newCard);
+
+            Assert.AreEqual(board.getCardsLeft(newCard), 10);
+        }
+        [TestMethod]
+        public void testVictoryCardAddedToBoard()
+        {
+            Dictionary<Card, int> cards = new Dictionary<Card, int>();
+            Queue<Player> turns = new Queue<Player>();
+            GameBoard board = new GameBoard(cards, turns);
+            Card newCard = new Estate();
+            board.addCard(newCard);
+
+            Assert.AreEqual(board.getCardsLeft(newCard), 10);
+        }
+        [TestMethod]
+        public void testTreasureCardAddedToBoard()
+        {
+            Dictionary<Card, int> cards = new Dictionary<Card, int>();
+            Queue<Player> turns = new Queue<Player>();
+            GameBoard board = new GameBoard(cards, turns);
+            Card newCard = new Copper();
+            board.addCard(newCard);
+
+            Assert.AreEqual(board.getCardsLeft(newCard), 10);
+        }
+    }
+}
---
+++ DominionGUI/UnitTestProject2/RandomCardsUnitTest.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using RandomGenerateCards;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class RandomCardsUnitTest
+    {
+        [TestMethod]
+        public void testRandomList1()
+        {
+            List<int> theList = GenerateRandom.GenerateRandomList(25, 15);
+            Boolean israndom = theList.Distinct().Count() == theList.Count();
+            Assert.AreEqual(true, israndom);
+        }
+        [TestMethod]
+        public void testRandomList2()
+        {
+            List<int> theList = GenerateRandom.GenerateRandomList(100, 10);
+            Boolean israndom = theList.Distinct().Count() == theList.Count();
+            Assert.AreEqual(true, israndom);
+        }
+
+        [TestMethod]
+        public void testSelectRandomWhenStartGame()
+        {
+            List<int> theList = GenerateRandom.GenerateRandomList(25, 5);
+            Boolean israndom = theList.Distinct().Count() == theList.Count();
+            Assert.AreEqual(true, israndom);
+        }
+
+        [TestMethod]
+        public void testSuffleDeckSize5()
+        {
+            ArrayList theList = new ArrayList();
+            theList.Add(4);
+            theList.Add(5);
+            theList.Add(11);
+            theList.Add(15);
+            theList.Add(29);
+
+            Stack olddeck = new Stack();
+
+            for (int i = 0; i < theList.Count; i++)
+            {
+                olddeck.Push(theList[i]);
+            }
+
+
+            Stack randomdeck = GenerateRandom.SuffleDeck(theList);
+
+            Boolean israndom = false;
+            for (int i = 0; i < olddeck.Count; i++)
+            {
+                if (olddeck.Pop() == randomdeck.Pop())
+                {
+                    israndom = false;
+                }
+                else
+                    israndom = true;
+            }
+            Assert.AreEqual(true, israndom);
+        }
+
+        [TestMethod]
+        public void testSuffleDeckSize10()
+        {
+            ArrayList theList = new ArrayList();
+            List<int> randomsize10list = GenerateRandom.GenerateRandomList(10, 10);
+            for (int i = 0; i < 10; i++)
+            {
+                theList.Add(randomsize10list[i]);
+            }
+
+            Stack olddeck = new Stack();
+
+            for (int i = 0; i < theList.Count; i++)
+            {
+                olddeck.Push(theList[i]);
+            }
+
+
+            Stack randomdeck = GenerateRandom.SuffleDeck(theList);
+
+            Boolean israndom = false;
+            while (olddeck.Count > 0)
+            {
+                if (olddeck.Pop() == randomdeck.Pop())
+                    israndom = false;
+                else
+                    israndom = true;
+            }
+            Assert.AreEqual(true, israndom);
+        }
+
+        [TestMethod]
+        public void testSuffleDeckSize100()
+        {
+            ArrayList theList = new ArrayList();
+            List<int> randomsize10list = GenerateRandom.GenerateRandomList(100, 100);
+            for (int i = 0; i < 100; i++)
+            {
+                theList.Add(randomsize10list[i]);
+            }
+
+            Stack olddeck = new Stack();
+
+            for (int i = 0; i < theList.Count; i++)
+            {
+                olddeck.Push(theList[i]);
+            }
+
+
+            Stack randomdeck = GenerateRandom.SuffleDeck(theList);
+
+            Boolean israndom = false;
+            while (olddeck.Count > 0)
+            {
+                if (olddeck.Pop() == randomdeck.Pop())
+                    israndom = false;
+                else
+                    israndom = true;
+            }
+            Assert.AreEqual(true, israndom);
+        }
+    }
+}
