6889883cb6e33c6de52f5ca1848d83b394c8d53b
Morgan <cookmn@rose-hulman.edu>
2015-04-23 20:26:22 -0400

    Wrote the minimal amount of code to pass

--- DominionGUI/DominionCards/KingdomCards/Adventurer.cs
+++ DominionGUI/DominionCards/KingdomCards/Adventurer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace DominionCards.KingdomCards
 {
@@ -14,7 +15,10 @@ namespace DominionCards.KingdomCards
         }
         public override void play(Player player)
         {
-            
+            player.drawHand();
+            player.addCardToHand(new Woodcutter());
+            player.addCardToHand(new Woodcutter());
+            Console.WriteLine(player.getHand().Count);
 
         }
     }
