10eaa4552a4879753cb426175af912eca5919d0f
Morgan <cookmn@rose-hulman.edu>
2015-05-07 15:37:38 -0400

    Created tieException class and a testing framework

---
+++ DominionGUI/DominionCards/TieException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominionCards
+{
+    public class TieException : Exception
+    {
+        public TieException(Player p1, Player p2) : base() 
+        {
+            // TODO
+        }
+        /**
+         * returns true if player broke the tie (beat the players that tied.)
+         * If the given player tied with all the players that were already tied, the player is added to the tie,
+         *   but false is returned.
+         * If the given player loses, false is returned.
+         */
+        public Boolean BreaksTie(Player p)
+        {
+            return false;
+        }
+    }
+}
---
+++ DominionGUI/UnitTestProject2/UnitTestException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTestException
+    {
+        [TestMethod]
+        public void BreakTieReturnsTrueIfNewPlayerWins()
+        {
+
+        }
+        [TestMethod]
+        public void BreakTieReturnsFalseIfNewPlayerLoses()
+        {
+
+        }
+        [TestMethod]
+        public void BreakTieAddsPlayerToTieIfNewPlayerTies()
+        {
+
+        }
+        [TestMethod]
+        public void BreakTieDoesNotAddPlayerToTieIfNewPlayerLoses()
+        {
+
+        }
+    }
+}
