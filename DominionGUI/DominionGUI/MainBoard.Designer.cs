f861be74057aef0e7e052f6d826b80d4b2631d5c
Luke Zhang <zhangl@rose-hulman.edu>
2015-05-13 22:17:19 -0400

    Commit

--- DominionGUI/DominionGUI/MainBoard.Designer.cs
+++ DominionGUI/DominionGUI/MainBoard.Designer.cs
@@ -44,17 +44,17 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(30, 486);
+            this.button1.Location = new System.Drawing.Point(30, 500);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(198, 109);
+            this.button1.Size = new System.Drawing.Size(154, 95);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(1712, 848);
+            this.button2.Location = new System.Drawing.Point(1787, 874);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(56, 32);
@@ -76,7 +76,7 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(1559, 802);
+            this.pictureBox2.Location = new System.Drawing.Point(1635, 817);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(127, 185);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -86,53 +86,58 @@
             // deck
             // 
             this.deck.AutoSize = true;
-            this.deck.Location = new System.Drawing.Point(96, 765);
+            this.deck.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.deck.Location = new System.Drawing.Point(51, 751);
             this.deck.Name = "deck";
-            this.deck.Size = new System.Drawing.Size(62, 13);
+            this.deck.Size = new System.Drawing.Size(148, 31);
             this.deck.TabIndex = 19;
-            this.deck.Text = "Deck Size: "+ DominionCards.GameBoard.getInstance().turnOrder.Peek().getDeck().Count;
+            this.deck.Text = "Deck size: " + DominionCards.GameBoard.getInstance().turnOrder.Peek().getDeck().Count;
             // 
             // actionleft
             // 
             this.actionleft.AutoSize = true;
-            this.actionleft.Location = new System.Drawing.Point(430, 765);
+            this.actionleft.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.actionleft.Location = new System.Drawing.Point(303, 751);
             this.actionleft.Name = "actionleft";
-            this.actionleft.Size = new System.Drawing.Size(64, 20);
+            this.actionleft.Size = new System.Drawing.Size(158, 31);
             this.actionleft.TabIndex = 20;
             this.actionleft.Text = "Action Left: ";
             // 
             // buyleft
             // 
             this.buyleft.AutoSize = true;
-            this.buyleft.Location = new System.Drawing.Point(591, 765);
+            this.buyleft.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.buyleft.Location = new System.Drawing.Point(681, 751);
             this.buyleft.Name = "buyleft";
-            this.buyleft.Size = new System.Drawing.Size(52, 20);
+            this.buyleft.Size = new System.Drawing.Size(129, 31);
             this.buyleft.TabIndex = 21;
             this.buyleft.Text = "Buy Left: ";
             // 
             // moneyleft
             // 
             this.moneyleft.AutoSize = true;
-            this.moneyleft.Location = new System.Drawing.Point(739, 765);
+            this.moneyleft.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.moneyleft.Location = new System.Drawing.Point(1067, 751);
             this.moneyleft.Name = "moneyleft";
-            this.moneyleft.Size = new System.Drawing.Size(66, 20);
+            this.moneyleft.Size = new System.Drawing.Size(163, 31);
             this.moneyleft.TabIndex = 22;
             this.moneyleft.Text = "Money Left: ";
             // 
             // discarddeck
             // 
             this.discarddeck.AutoSize = true;
-            this.discarddeck.Location = new System.Drawing.Point(1592, 765);
+            this.discarddeck.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.discarddeck.Location = new System.Drawing.Point(1579, 751);
             this.discarddeck.Name = "discarddeck";
-            this.discarddeck.Size = new System.Drawing.Size(62, 20);
+            this.discarddeck.Size = new System.Drawing.Size(248, 31);
             this.discarddeck.TabIndex = 23;
-            this.discarddeck.Text = "Discard Deck Size: ";
+            this.discarddeck.Text = "Discard Deck size: ";
             // 
             // MainBoard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1809, 1062);
+            this.ClientSize = new System.Drawing.Size(1874, 1054);
             this.Controls.Add(this.discarddeck);
             this.Controls.Add(this.deck);
             this.Controls.Add(this.actionleft);
