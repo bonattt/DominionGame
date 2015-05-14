d7f0d907bbeafed1791ae4598527ec8f760999a9
Morgan <cookmn@rose-hulman.edu>
2015-05-14 17:22:54 -0400

    Created a pop up checkbox feature

--- DominionGUI/DominionCards/KingdomCards/ThroneRoom.cs
+++ DominionGUI/DominionCards/KingdomCards/ThroneRoom.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,7 @@ namespace DominionCards.KingdomCards
         public override void Play(Player player)
         {
             base.Play(player);
-            Form1 form = new Form1();
+            Form1 form = new Form1(player.getHand(), "Select one card to trash", 1);
             form.TopMost = true;
             form.ShowDialog();
 
